eb6f8c1f1a30a2ad9d47ed1a909bc41ca907d541
Siddhartha Dikshit <nivogroup@gmail.com>
2020-06-07 07:27:30 +0530

    Initial update

---
+++ Persistence/DataContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataContextSeed
+    {
+		public static async Task SeedAsync(DataContext context, ILoggerFactory loggerFactory)
+		{
+			try
+			{
+				var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+				if (!context.Values.Any())
+				{
+					var valuesData = File.ReadAllText(@"/SeedData/values.json");
+					var brands = JsonConvert.DeserializeObject<List<Value>>(valuesData);
+					foreach (var item in brands)
+					{
+						context.Values.Add(item);
+					}
+					await context.SaveChangesAsync();
+				}
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<DataContextSeed>();
+				logger.LogError(ex.Message);
+			}
+		}
+	}
+}
