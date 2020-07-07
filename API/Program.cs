9bd6cbbeff5a8de63cb0571ab6dc69de1936c4da
Siddhartha Dikshit <nivogroup@gmail.com>
2020-07-07 17:28:58 +0530

    CRUD API

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -25,6 +22,7 @@ namespace API
                 {
                     var context = services.GetRequiredService<DataContext>();
                     await context.Database.MigrateAsync();
+                    Seeds.SeedData(context);
                     //await DataContextSeed.SeedAsync(context, loggerFactory);
 
                 }
