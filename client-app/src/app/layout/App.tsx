1aa07aa74b519c13e1d63f9cb47ca453240fe73d
Siddhartha Dikshit <nivogroup@gmail.com>
2020-07-10 08:13:48 +0530

    Mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,92 +1,31 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
+import React, { useEffect, Fragment, useContext } from 'react';
 import { Container } from 'semantic-ui-react';
-import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
 import LoadingComponents from './LoadingComponents';
+import ActivityStore from '../stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 
 
 const App = () => {
-  const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
-  const [editMode, setEditMode] = useState(false);
-  
-  //for loading
-  const [loading, setLoading] = useState(true);
-  const [submitting, setsubmitting] = useState(false);
-
-  const [target, setTarget] = useState('');
-
-  const handleSelectedActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
-    setEditMode(false);
-  };
-
-  const handleOpenCreateForm = () => {
-    setSelectedActivity(null);
-    setEditMode(true);
-  }
-
-  const handleCreateActivity = (activity: IActivity) => {
-    setsubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setsubmitting(false))
-  }
-
-  const handleEditActivity = (activity: IActivity) => {
-    setsubmitting(true);
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setsubmitting(false))
-  }
-
-  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
-    setsubmitting(true);
-    setTarget(event.currentTarget.name);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)])
-    }).then(() => setsubmitting(false))
-  }
+  const activityStore = useContext(ActivityStore)
 
   useEffect(() => {
-    agent.Activities.list()
-      .then((response) => {
-        let activities: IActivity[] = [];
-        response.forEach(activity => {
-          activity.date = activity.date.split('.')[0];
-          activities.push(activity);
-        })
-        setActivities(activities);
-      }).then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if (loading) return <LoadingComponents content='Loading activities....'/>
+  if (activityStore.loadingInitial) return <LoadingComponents content='Loading activities....'/>
 
   return (
     <Fragment>
-      <NavBar openCreateForm={handleOpenCreateForm}/>
+      <NavBar/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-          activities={activities} 
-          selectActivity={handleSelectedActivity} selectedActivity={selectedActivity} setSelectedActivity={setSelectedActivity}
-          editMode = {editMode} setEditMode = {setEditMode}
-          createActivity={handleCreateActivity} editActivity={handleEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-          target={target}
-
-        />
+        <ActivityDashboard />
       </Container>
     </Fragment>
   );
 
 }
 
-export default App;
+export default observer(App);
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import 'mobx-react-lite/batchingForReactDom';
 
 ReactDOM.render(
   //<React.StrictMode>
