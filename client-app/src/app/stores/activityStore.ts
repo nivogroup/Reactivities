d2c26623d9310b4f5c20fd2b9ff367e94ff0bd17
Siddhartha Dikshit <nivogroup@gmail.com>
2020-07-15 19:31:21 +0530

    Form submit and validation

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,8 @@ import {observable, action, computed, configure, runInAction} from 'mobx';
 import { createContext, SyntheticEvent } from 'react';
 import { IActivity } from '../models/activity';
 import agent from '../api/agent';
+import { history } from '../..';
+import { toast } from 'react-toastify';
 
 configure({enforceActions: 'always'});
 
@@ -18,10 +20,10 @@ export class ActivityStore {
 
     groupActivitiesByDate(activities: IActivity[]) {
         const sortedActivities = activities.sort(
-            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+            (a, b) => a.date.getTime() - b.date.getTime()
         )
         return Object.entries(sortedActivities.reduce((activities, activity) => {
-            const date = activity.date.split('T')[0];
+            const date = activity.date.toISOString().split('T')[0];
             activities[date] = activities[date] ? [...activities[date], activity] : [activity];
             return activities;
 
@@ -34,7 +36,7 @@ export class ActivityStore {
             const activities = await agent.Activities.list();
             runInAction('loading activities', () => {
                 activities.forEach(activity => {
-                    activity.date = activity.date.split('.')[0];
+                    activity.date = new Date(activity.date);
                     this.activityRegistry.set(activity.id, activity);
                 });
                 this.loadingInitial = false;
@@ -51,14 +53,18 @@ export class ActivityStore {
         let activity = this.getActivity(id);
         if(activity) {
             this.activity = activity
+            return activity;
         } else {
             this.loadingInitial = true;
             try {
                 activity = await agent.Activities.details(id);
                 runInAction('getting activity', () => {
+                    activity.date = new Date(activity.date);
                     this.activity = activity;
+                    this.activityRegistry.set(activity.id, activity);
                     this.loadingInitial = false;
                 })
+                return activity;
             } catch (error) {
                 runInAction('Getting activity Error', () => {
                     this.loadingInitial = false;
@@ -83,13 +89,15 @@ export class ActivityStore {
             runInAction('creating activity', () => {
                 this.activityRegistry.set(activity.id, activity);
                 this.submitting = false;
-            })
+            });
+            history.push(`/activities/${activity.id}`);
             
         } catch (error) {
             runInAction('creating activity error', () => {
                 this.submitting = false;
             })
-            console.log(error);
+            toast.error('Problem Submitting Data');
+            console.log(error.response);
         }
     }
 
@@ -101,12 +109,14 @@ export class ActivityStore {
                 this.activityRegistry.set(activity.id, activity);
                 this.activity =activity;
                 this.submitting = false;
-            }) 
+            });
+            history.push(`/activities/${activity.id}`); 
         } catch (error) {
             runInAction('edit activity error', () => {
                 this.submitting = false;
             })
-            console.log(error);
+            toast.error('Problem Submitting Data');
+            console.log(error.response);
         }
     }
 
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -8,7 +8,11 @@ import 'mobx-react-lite/batchingForReactDom';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-widgets/dist/css/react-widgets.css';
 import ScrollToTop from './app/layout/ScrollToTop';
+import dateFnsLocalizer from 'react-widgets-date-fns';
+
+dateFnsLocalizer()
 
 export const history = createBrowserHistory();
 
